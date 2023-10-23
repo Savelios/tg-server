0ab545ec753798b2f707ee9a13fac3f32e5ba420
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-10-23 10:56:40 +0300

    add

--- index.js
+++ index.js
@@ -50,29 +50,22 @@ bot.on("message", async (msg) => {
   }
 });
 
-app.post("/web-data", async (req, res) => {
-  const { queryId, products, totalPrice } = req.body;
-
-  try {
-    await bot.answerWebAppQuery(queryId, {
-      type: "article",
-      id: queryId,
-      title: "Успешная покупка",
-      input_message_content: {
-        message_text: "Вы приобрели товар на сумму" + totalPrice,
-      },
-    });
-    return res.status(200).json({});
-  } catch (e) {
-    await bot.answerWebAppQuery(queryId, {
-      type: "article",
-      id: queryId,
-      title: "Не удалось приобрести товар",
-      input_message_content: { message_text: "Не удалось приобрести товар" },
-    });
-    return res.status(500).json({});
-  }
-});
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
 
 const PORT = 8000;
 app.listen(PORT, () => console.log("server started on PORT" + PORT));
